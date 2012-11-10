32e70c635aedba1d04d3ebb79f905b784ac0b09f
Daniel Keir Haywood <danhaywood@apache.org>
2012-11-10 18:11:48 +0000

    more experimentation
    git-svn-id: https://svn.apache.org/repos/asf/isis/site/trunk@1407864 13f79535-47bb-0310-9956-ffa450edef68

--- lib/view.pm
+++ lib/view.pm
@@ -1,6 +1,55 @@
 package view;
 use base 'ASF::View'; # see https://svn.apache.org/repos/infra/websites/cms/build/lib/ASF/View.pm
 
+
+# A "basic" view, which takes 'template' and 'path' parameters.
+
+sub basic {
+    my %args = @_;
+    my $filepath = "content$args{path}";
+
+    print "basic $filepath";
+
+    read_text_file($filepath, \%args);
+
+    $args{path} =~ s/\.mdtext$/\.html/;
+    $args{base} = _base($args{path});
+    $args{breadcrumbs} = _breadcrumbs($args{path}, $args{base});
+
+    my $template_path = "templates/$args{template}";
+
+    my @includes = ($args{content} =~ m/{include:([^ ]+?)}/g);
+
+    foreach my $include (@includes) {
+        next unless ( -e "content/$include");
+
+        my %a = ();
+        read_text_file("content/$include", \%a);
+        my $text = $a{content};
+        $args{headers}{title} = $a{headers}{title} unless $args{headers}{title};
+
+        # If the file to be included is in a child directory, resolve all the links
+        # in the included content to be relative to this document
+        if ($include =~ m,/,) {
+            my $ipath = $include;
+            $ipath =~ s,/[^/]*$,,;
+            $text =~ s,(\[[^[]+])\(([^/][^)]+)\),$1($ipath/$2),g;
+        }
+
+        $args{content} =~ s/{include:$include}/$text/g;
+    }
+
+    print " - rendering";
+
+    my $rendered = Dotiac::DTL->new($template_path)->render(\%args);
+
+    print " - complete\n";
+
+    return ($rendered, 'html', \%args);
+}
+
+
+
 1;
 
 =head1 LICENSE
