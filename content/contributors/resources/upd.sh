c9400c6798fa9806fc26d640403192d819978dd5
Daniel Keir Haywood <danhaywood@apache.org>
2014-01-08 01:25:05 +0000

    md5 for downloads
    git-svn-id: https://svn.apache.org/repos/asf/isis/site/trunk@1556421 13f79535-47bb-0310-9956-ffa450edef68

--- content/contributors/resources/upd.sh
+++ content/contributors/resources/upd.sh
@@ -1,12 +1,12 @@
 #################################################################
 #
 # update the variables in this first section as required; 
-# format is "$old_ver new_ver"
+# format is "old_ver new_ver"
 #
 # eg:
-# archetype_todoapp="1.3.0 1.4.0"   # update from 1.3.0 to 1.4.0
-# archetype_todoapp="1.4.0 1.4.0"   # don't update (since old_ver = new_ver)
-# archetype_todoapp=""              # don't update (no versions provided)
+# isis_core="1.3.0 1.4.0"   # update from 1.3.0 to 1.4.0
+# isis_core="1.4.0 1.4.0"   # don't update (since old_ver = new_ver)
+# isis_core=""              # also don't update (no versions provided)
 #
 # It shouldn't be necessary to update anything else.
 #
@@ -19,13 +19,12 @@ security_file="1.0.1 1.0.1"
 viewer_restfulobjects="2.1.0 2.1.0"
 viewer_wicket="1.3.1 1.3.1"
 archetype_todoapp="1.3.1 1.3.1"
+archetype_simple="1.3.1 1.3.1"
 
 
 
 #################################################################
-#
 # constants
-#
 #################################################################
 repo_root=https://repository.apache.org/content/repositories/releases/org/apache/isis
 
@@ -37,7 +36,9 @@ md5="$zip.md5"
 
 
 #################################################################
+#
 # isis_core
+#
 #################################################################
 old_ver=`echo $isis_core | awk '{print $1}'
 new_ver=`echo $isis_core | awk '{print $2}'
@@ -65,7 +66,9 @@ fi
 
 
 #################################################################
+#
 # objectstore_jdo
+#
 #################################################################
 old_ver=`echo $objectstore_jdo | awk '{print $1}'
 new_ver=`echo $objectstore_jdo | awk '{print $2}'
@@ -95,7 +98,9 @@ fi
 
 
 #################################################################
+#
 # security_file
+#
 #################################################################
 old_ver=`echo $security_file | awk '{print $1}'
 new_ver=`echo $security_file | awk '{print $2}'
@@ -125,7 +130,9 @@ fi
 
 
 #################################################################
+#
 # security_shiro
+#
 #################################################################
 old_ver=`echo $security_shiro | awk '{print $1}'
 new_ver=`echo $security_shiro | awk '{print $2}'
@@ -155,7 +162,9 @@ fi
 
 
 #################################################################
+#
 # viewer_restfulobjects
+#
 #################################################################
 old_ver=`echo $viewer_restfulobjects | awk '{print $1}'
 new_ver=`echo $viewer_restfulobjects | awk '{print $2}'
@@ -185,7 +194,9 @@ fi
 
 
 #################################################################
+#
 # viewer_wicket
+#
 #################################################################
 old_ver=`echo $viewer_wicket | awk '{print $1}'
 new_ver=`echo $viewer_wicket | awk '{print $2}'
@@ -215,7 +226,9 @@ fi
 
 
 #################################################################
+#
 # archetype_todoapp
+#
 #################################################################
 old_ver=`echo $archetype_todoapp | awk '{print $1}'
 new_ver=`echo $archetype_todoapp | awk '{print $2}'
@@ -240,4 +253,32 @@ if [ "$old_ver" != "$new_ver" ]; then
 	popd
 fi
 
+
+#################################################################
+#
+# archetype_simple
+#
+#################################################################
+old_ver=`echo $archetype_simple | awk '{print $1}'
+new_ver=`echo $archetype_simple | awk '{print $2}'
+
+if [ "$old_ver" != "$new_ver" ]; then 
+
+	type="archetype"
+	fullname="simple_wicket_restful_jdo-archetype"
+	pushd $type/$fullname
+
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$md5
+	svn add $fullname-$new_ver-$md5
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$asc
+	svn add $fullname-$new_ver-$asc
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$zip
+	svn add $fullname-$new_ver-$zip
+
+	svn delete $fullname-$old_ver-$md5
+	svn delete $fullname-$old_ver-$asc
+	svn delete $fullname-$old_ver-$zip
+
+	popd
+fi
 
