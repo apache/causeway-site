acb1595e0ea941442e0b94b63e4fc496bf216cfc
Daniel Keir Haywood <danhaywood@apache.org>
2014-01-08 01:18:09 +0000

    md5 for downloads
    git-svn-id: https://svn.apache.org/repos/asf/isis/site/trunk@1556419 13f79535-47bb-0310-9956-ffa450edef68

---
+++ content/contributors/resources/upd.sh
@@ -0,0 +1,243 @@
+#################################################################
+#
+# update the variables in this first section as required; 
+# format is "$old_ver new_ver"
+#
+# eg:
+# archetype_todoapp="1.3.0 1.4.0"   # update from 1.3.0 to 1.4.0
+# archetype_todoapp="1.4.0 1.4.0"   # don't update (since old_ver = new_ver)
+# archetype_todoapp=""              # don't update (no versions provided)
+#
+# It shouldn't be necessary to update anything else.
+#
+#################################################################
+
+isis_core="1.3.0 1.3.0"
+objectstore_jdo="1.3.0 1.3.0"
+security_shiro="1.3.0 1.3.0"
+security_file="1.0.1 1.0.1"
+viewer_restfulobjects="2.1.0 2.1.0"
+viewer_wicket="1.3.1 1.3.1"
+archetype_todoapp="1.3.1 1.3.1"
+
+
+
+#################################################################
+#
+# constants
+#
+#################################################################
+repo_root=https://repository.apache.org/content/repositories/releases/org/apache/isis
+
+zip="source-release.zip"
+asc="$zip.asc"
+md5="$zip.md5"
+
+
+
+
+#################################################################
+# isis_core
+#################################################################
+old_ver=`echo $isis_core | awk '{print $1}'
+new_ver=`echo $isis_core | awk '{print $2}'
+
+if [ "$old_ver" != "$new_ver" ]; then 
+
+	type="core"
+	fullname="isis"
+	pushd isis-core
+
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$asc
+	svn add $fullname-$new_ver-$asc
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$md5
+	svn add $fullname-$new_ver-$md5
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$zip
+	svn add $fullname-$new_ver-$zip
+
+	svn delete $fullname-$old_ver-$asc
+	svn delete $fullname-$old_ver-$md5
+	svn delete $fullname-$old_ver-$zip
+
+	popd
+fi
+
+
+
+#################################################################
+# objectstore_jdo
+#################################################################
+old_ver=`echo $objectstore_jdo | awk '{print $1}'
+new_ver=`echo $objectstore_jdo | awk '{print $2}'
+
+if [ "$old_ver" != "$new_ver" ]; then 
+
+	type="objectstore"
+	localname="jdo"
+
+	fullname="isis-$type-$localname"
+	pushd component/$type/$localname"
+
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$asc
+	svn add $fullname-$new_ver-$asc
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$md5
+	svn add $fullname-$new_ver-$md5
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$zip
+	svn add $fullname-$new_ver-$zip
+
+	svn delete $fullname-$old_ver-$asc
+	svn delete $fullname-$old_ver-$md5
+	svn delete $fullname-$old_ver-$zip
+
+	popd
+fi
+
+
+
+#################################################################
+# security_file
+#################################################################
+old_ver=`echo $security_file | awk '{print $1}'
+new_ver=`echo $security_file | awk '{print $2}'
+
+if [ "$old_ver" != "$new_ver" ]; then 
+
+	type="security"
+	localname="file"
+
+	fullname="isis-$type-$localname"
+	pushd component/$type/$localname"
+
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$asc
+	svn add $fullname-$new_ver-$asc
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$md5
+	svn add $fullname-$new_ver-$md5
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$zip
+	svn add $fullname-$new_ver-$zip
+
+	svn delete $fullname-$old_ver-$asc
+	svn delete $fullname-$old_ver-$md5
+	svn delete $fullname-$old_ver-$zip
+
+	popd
+fi
+
+
+
+#################################################################
+# security_shiro
+#################################################################
+old_ver=`echo $security_shiro | awk '{print $1}'
+new_ver=`echo $security_shiro | awk '{print $2}'
+
+if [ "$old_ver" != "$new_ver" ]; then 
+
+	type="security"
+	localname="shiro"
+
+	fullname="isis-$type-$localname"
+	pushd component/$type/$localname"
+
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$asc
+	svn add $fullname-$new_ver-$asc
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$md5
+	svn add $fullname-$new_ver-$md5
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$zip
+	svn add $fullname-$new_ver-$zip
+
+	svn delete $fullname-$old_ver-$asc
+	svn delete $fullname-$old_ver-$md5
+	svn delete $fullname-$old_ver-$zip
+
+	popd
+fi
+
+
+
+#################################################################
+# viewer_restfulobjects
+#################################################################
+old_ver=`echo $viewer_restfulobjects | awk '{print $1}'
+new_ver=`echo $viewer_restfulobjects | awk '{print $2}'
+
+if [ "$old_ver" != "$new_ver" ]; then 
+
+	type="viewer"
+	localname="restfulobjects"
+
+	fullname="isis-$type-$localname"
+	pushd component/$type/$localname"
+
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$asc
+	svn add $fullname-$new_ver-$asc
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$md5
+	svn add $fullname-$new_ver-$md5
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$zip
+	svn add $fullname-$new_ver-$zip
+
+	svn delete $fullname-$old_ver-$asc
+	svn delete $fullname-$old_ver-$md5
+	svn delete $fullname-$old_ver-$zip
+
+	popd
+fi
+
+
+
+#################################################################
+# viewer_wicket
+#################################################################
+old_ver=`echo $viewer_wicket | awk '{print $1}'
+new_ver=`echo $viewer_wicket | awk '{print $2}'
+
+if [ "$old_ver" != "$new_ver" ]; then 
+
+	type="viewer"
+	localname="wicket"
+
+	fullname="isis-$type-$localname"
+	pushd component/$type/$localname"
+
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$asc
+	svn add $fullname-$new_ver-$asc
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$md5
+	svn add $fullname-$new_ver-$md5
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$zip
+	svn add $fullname-$new_ver-$zip
+
+	svn delete $fullname-$old_ver-$asc
+	svn delete $fullname-$old_ver-$md5
+	svn delete $fullname-$old_ver-$zip
+
+	popd
+fi
+
+
+
+#################################################################
+# archetype_todoapp
+#################################################################
+old_ver=`echo $archetype_todoapp | awk '{print $1}'
+new_ver=`echo $archetype_todoapp | awk '{print $2}'
+
+if [ "$old_ver" != "$new_ver" ]; then 
+
+	type="archetype"
+	fullname="quickstart_wicket_restful_jdo-archetype"
+	pushd $type/$fullname
+
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$md5
+	svn add $fullname-$new_ver-$md5
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$asc
+	svn add $fullname-$new_ver-$asc
+	curl -O $repo_root/$type/$fullname/$new_ver/$fullname-$new_ver-$zip
+	svn add $fullname-$new_ver-$zip
+
+	svn delete $fullname-$old_ver-$md5
+	svn delete $fullname-$old_ver-$asc
+	svn delete $fullname-$old_ver-$zip
+
+	popd
+fi
+
+
