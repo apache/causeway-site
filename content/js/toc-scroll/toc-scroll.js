dcaca65c657d9aa0efb7aafd4fd1f49b9af4c5ef
Dan Haywood <dan@haywood-associates.co.uk>
2017-05-12 11:05:45 +0100

    ISIS-1594: fixes edit button for subdocuments

--- content/js/toc-scroll/toc-scroll.js
+++ content/js/toc-scroll/toc-scroll.js
@@ -3,13 +3,21 @@ $( document ).ready(function() {
     $("ul.nav li a").click(function (event) {
         var $target = $(event.currentTarget),
             $scrollToTarget = $($target.attr("href")),
-            $header = $("nav.navbar-fixed-top"),
+            $header = $("nav.navbar-static-top"),
             prop = {
-                scrollTop:   $scrollToTarget.offset().top - $header.outerHeight(true)
+                // subtracting the offset results in the incorrect entry in the TOC from being highlighted.
+                // since the sticky-header keeps the header out of the way, this isn't a problem for us.
+                scrollTop:   $scrollToTarget.offset().top /*- $header.outerHeight(true)*/
             },
             speed = 500;
 
-        $('html, body').animate(prop, speed);
+
+        $($header).attr("data-toc-scrolling", "true")
+        $('html, body').animate(prop, speed,
+            function() {
+                var x = 2;
+                $($header).removeAttr("data-toc-scrolling")
+            });
     });
 
 });
