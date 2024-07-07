e836199f022a353c5f1f08030fbdf4cc35aac9d3
Dan Haywood <dan@haywood-associates.co.uk>
2024-07-07 23:11:07 +0100

    updates script
    remove winpty

--- update-search-index.sh
+++ update-search-index.sh
@@ -1,2 +1,2 @@
 cd content
-winpty docker run -it --env-file=../algolia.env -e "CONFIG=$(cat ../algolia-config.json | jq -r tostring)" algolia/docsearch-scraper
+docker run -it --env-file=../algolia.env -e "CONFIG=$(cat ../algolia-config.json | jq -r tostring)" algolia/docsearch-scraper
